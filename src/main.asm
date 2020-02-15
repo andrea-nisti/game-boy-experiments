e22654c03a9f2ebebc2f76c5ba5f2caaf4b53c94
Andrea Nisticò <andrea.nistic@gmail.com>
2020-02-15 02:05:10 +0100

    add a tile and some comments

--- src/main.asm
+++ src/main.asm
@@ -21,7 +21,7 @@ Start:
     xor a; ld a, 0
     ld [rLCDC], a
 
-    ld hl, $9000
+    ld hl, $9000; copy font inside VRAM (starts at $8000, apparently, but we copy at the end)
     ld de, FontTiles
     ld bc, FontTilesEnd - FontTiles
 
@@ -35,13 +35,13 @@ Start:
     jr nz, .copyFont
 
     ld hl, $9901; print sentence on top screen
-    ld de, HelloWorldStr
+    ld de, HelloWorldStr; load destination address
 
 .copyString
     ld a, [de]
     ld [hli], a
     inc de
-    and a
+    and a;check for end char
     jr nz, .copyString
 
     ld a, %11100100
@@ -54,6 +54,9 @@ Start:
     ld a, %10000001
     ld [rLCDC], a
 
+TileLoop:
+
+
 .lockup; infinite loop
     jr .lockup
 
@@ -61,9 +64,17 @@ SECTION "Font", ROM0
 
 FontTiles:
 INCBIN "font.chr"
+
 FontTilesEnd:
 
 SECTION "Hello World string", ROM0
 
 HelloWorldStr:
     db "Hello Retro World", 0
+
+SECTION "Tiles", ROM0
+
+Tile:; Start of tile array.
+    DB $FF,$FF,$81,$81,$A5,$81,$81,$80
+    DB $81,$84,$81,$18,$81,$00,$7E,$00
+TileEnd:
